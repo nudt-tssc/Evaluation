d98f529ef9db9243c9d200e0e960f928777eda11
wingrew <1934247510@qq.com>
2025-08-28 23:19:03 +0800

    2

--- cpu/cpu.test.js
+++ cpu/cpu.test.js
@@ -2,23 +2,23 @@
 let points = {
     'cpu opt': [0, 4992]
 };
-const { assert } = require('console');
-const fs = require('fs');  // 引入 fs 模块
+// const { assert } = require('console');
+// const fs = require('fs');  // 引入 fs 模块
 
-// 读取命令行传入的文件路径（在命令行中传入文件路径）
-const filePath = process.argv[2];  // 通过 process.argv 获取传入的文件路径
+// // 读取命令行传入的文件路径（在命令行中传入文件路径）
+// const filePath = process.argv[2];  // 通过 process.argv 获取传入的文件路径
 
-//读取文件内容
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error("读取文件时发生错误:", err);
-        return;
-    }
+// //读取文件内容
+// fs.readFile(filePath, 'utf8', (err, data) => {
+//     if (err) {
+//         console.error("读取文件时发生错误:", err);
+//         return;
+//     }
 
-    // 将文件内容传递给 judge 函数
-    let result = judge(data);
-    console.log("Judge function result:", result);  // 输出 judge 函数的返回值
-});
+//     // 将文件内容传递给 judge 函数
+//     let result = judge(data);
+//     console.log("Judge function result:", result);  // 输出 judge 函数的返回值
+// });
 // // Base assertion utility function
 
 // Main judge function
